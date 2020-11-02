707919758a9607a601c37522a61c41888c312621
Bryan <bmoreno.dl@gmail.com>
2020-11-02 12:06:32 -0500

    creates ads for project display

--- email_templates/templates.js
+++ email_templates/templates.js
@@ -1,23 +1,27 @@
 exports.resetPassword = (user,token)=>{
     return `
-    <div style="border:2px solid #262626;width:95%;margin:30px auto;padding:1rem">
+    <div style="border:2px solid #262626;width:95%;margin:30px auto;padding:1rem">       
         <h1>Hola ${user.firstName},</h1>
         <br>
         <br>
         <p>Puedes reestablecer tu clave de acceso dandole click al siguente vinculo: </p>
         <p> Nombre de usuario: ${user.username} </p> 
         <br> 
-        <a href="http://${process.env.FRONT}/#/nueva_clave/${token.token}">Reestablecer clave </a>
+        <a href="${process.env.FRONT}/#/nueva_clave/${token.token}">Reestablecer clave </a>
     </div>
     `
 };
 
 exports.firstValidation = (user,token)=>{
-    return `<h1>Hola ${user.firstName},</h1>
-    <br>
-    <br>
-    <p>Por favor verifica tu cuenta dandole click al siguente vinculo: </p>
-    <br>
-    <a href="http://${process.env.FRONT}/#/confirmation/${token.token}">Validar Correo Electronico </a> `
+    return `
+    <div style="border:2px solid #262626;width:95%;margin:30px auto;padding:1rem">        
+        <h1>Hola ${user.firstName},</h1>
+        <br>
+        <br>
+        <p>Por favor verifica tu cuenta dandole click al siguente vinculo: </p>
+        <br> 
+        <a href="${process.env.FRONT}/#/confirmation/${token.token}">Validar Correo Electronico </a> 
+    </div>
+    `
 
 }
